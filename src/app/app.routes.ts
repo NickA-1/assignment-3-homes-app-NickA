969b42fdce5b26f5db5c307b8d73c6bf671311c1
NickA-1 <nick.ms.althoff@gmail.com>
2026-03-09 17:37:23 -0500

    Completed Milestone 9 (Added routing to the angular application)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Details } from './details/details';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: Home, title: 'Homepage'},
+    {path: 'details', component: Details, title: 'Home Details'},
+];
